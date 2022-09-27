0040f59e2d94f72ed7abcceca396ecc637c49848
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-09-27 08:46:24 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -9,7 +9,14 @@ def publishArtifacts() {
   stage('Prepare Artifacts') {
     if (env.APP_TYPE == "nodejs")
     sh """
-      zip -r ${COMPONENT}.zip node_modules server.js
+      zip -r ${COMPONENT}-${TAG_NAME}.zip node_modules server.js
     """
+  }
+  stage('Push artifacts to Nexus') {
+    withCredentials([usernamePassword(credentialsId: 'nexus', passwordVariable: 'pass', usernameVariable: 'user')]) {
+      sh """
+        curl -v -u ${user}:${pass} --upload-file ${COMPONENT}-${TAG_NAME}.zip  http://nexus.roboshop.internal:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
+      """
+    }
   }
 }
