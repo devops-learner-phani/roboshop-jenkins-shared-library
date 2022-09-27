3c13c90425887e9f9f3e0d0c90b499dc013b25f3
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-09-27 12:43:47 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/golang.groovy
+++ vars/golang.groovy
@@ -1,18 +1,18 @@
-def call() {
-  node() {
-    
-    common.pipelineInit()
-    stage('Download dependencies') {
-      sh '''
-        go mod init dispatch
-        go get
-        go build
-      '''
-    }
-
-    if (env.BRANCH_NAME == env.TAG_NAME)
-    {
-      common.publishArtifacts()
-    }
-  }
-}    
+//def call() {
+//  node() {
+//
+//    common.pipelineInit()
+//    stage('Download dependencies') {
+//      sh '''
+//        go mod init dispatch
+//        go get
+//        go build
+//      '''
+//    }
+//
+//    if (env.BRANCH_NAME == env.TAG_NAME)
+//    {
+//      common.publishArtifacts()
+//    }
+//  }
+//}
